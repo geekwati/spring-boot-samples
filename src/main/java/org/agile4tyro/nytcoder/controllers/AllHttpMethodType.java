a242cbbf29a9dd7ec72b4a3eccd44808bcfbff57
jyoti <jyoti06goel@gmail.com>
2018-10-02 15:17:59 +0530

    http methods

--- src/main/java/org/agile4tyro/nytcoder/controllers/AllHttpMethodType.java
+++ src/main/java/org/agile4tyro/nytcoder/controllers/AllHttpMethodType.java
@@ -1,15 +1,39 @@
+
 package org.agile4tyro.nytcoder.controllers;
 
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 @RestController
 public class AllHttpMethodType {
+	
+	
     @RequestMapping("/")
     public String welcome() {
         return "welcome";
+    }
+    
+    @RequestMapping(value ="getmethod", method = RequestMethod.GET)
+    public String getMethod() {
+    	return "Http Get Method";
+    }
+    
+    @RequestMapping(value = "postmethod", method = RequestMethod.POST)
+    public void postMethod(@RequestBody String str) {
+    	System.out.println("Post Method = "+ str);
+    }
+    
+    @RequestMapping(value= "putmethod", method = RequestMethod.PUT)
+    //public String putMethod(@RequestBody Long number) {
+    public String putMethod() {
+    	return "Put Http Method";
+    }
+    
+    @RequestMapping(value = "deletemethod", method = RequestMethod.DELETE)
+    public Boolean deleteMethod() {
+    	return true;
     }
 }
